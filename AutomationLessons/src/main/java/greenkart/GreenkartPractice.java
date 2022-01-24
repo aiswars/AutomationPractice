bc8d7d00f3152b6a74ec65a486f7ca5409ce08af
aisdhar <75646664+aisdhar@users.noreply.github.com>
2022-01-24 15:33:08 -0500

    counter j outside for loop

--- AutomationLessons/src/main/java/greenkart/GreenkartPractice.java
+++ AutomationLessons/src/main/java/greenkart/GreenkartPractice.java
@@ -9,32 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class greenkartpractice {
-
-	//1. To Locate the elements to add use findelements-- List of elements having the css -- a list
-	//2. products.size(); >> gives the size/count of products in the list
-	//3. getting name of product at index i
-	//4. Cucumber - 1 Kg - 3& 4 --type the name and compare the name --use ignoreCase or contains --As name is with 1kg its better not to use ignoreCase
-		 // compare the name -- to check for the required product 			 * 
-	//5. click on Add to Cart 
-	//6.  break; // once the required product is added --no need to check for the remaining // prodcuts -- break the for loop 
-	//7. if more no of products to add or to add more prodcuts any time -- to make the program generic use array to store the products-- keep on adding products
-			// here as needed-- no change required to the code --just add the product names here
-	//8. 4 to 8 convert array to arraylist - array takes less memory so storing the required products in an Array  -
-	     //and at run time coverting to arraylist as its easier to get the products--google the conversion
-	//9. //4 to 9- check whether name you extracted is present in arrayList or not// Arraylist also has contains method
-	//10. 5 to 10 //click on Add to cart
-	//11. Cucumber - 1 Kg -the product name in application is like these- so add // like these in the products array or split it
-	         //split returns 2 values in array -- returns an array with 2 values -- with right value and left value
-				// name = Cucumber - 1 Kg		// name1[0] = Cucumber	// name1[1] = 1 Kg
-	//12. to trim the white space on bothsides
-	//13. //9 to 13 compare the name -- to check for the required product
-	//14. //6 to 14 -- once the required product is added --no need to check for the remaining prodcuts -- to break the for loop use a counter
-	      //size of the array - no of required products in the array{break; 
-	      //once the required product is added - to break the for loop use a counter
-	//15.	10 to 15// xpath by text() -// but the text	// change once it is // clicked-- ADD TO CART to ADDED TO CART--SO ITS	// Better not to use text() -// locator text
-	      //ADD TO CART index is changed here when driver coming to locate the next product
-	//16. 	Thread.sleep(3000);
+public class GreenkartPractice {
 
 	public static void main(String[] args) throws InterruptedException {
 
@@ -44,20 +19,22 @@ public class greenkartpractice {
 				String[] vegNeeded = { "Cucumber", "Brocolli", "Beetroot" ,"Pumpkin", "Banana"};// required products in array
 				driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 		      	Thread.sleep(3000);
+		        
+		      	int j = 0; // to break the for loop once the products in the array are added to cart use a counter
 				
 				// 1 // List of elements having this css // Locate the elements 
-				List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name")); 
+				List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name")); //list for located products
 				// 2 products.size(); >> gives the size/count of products in the list
 				for (int i = 0; i < products.size(); i++) // iterate through the list and check the name of the product
 				{ // 3
 					String productName = products.get(i).getText();// name of product at index i		// 11.format it to get actual vegetable name
 					String[] productNameSplit = productName.split("-");// returns an array with right value and left value name = Cucumber - 1 Kg name1[0] = Cucumber name1[1] = 1 Kg
 		            String vegName = productNameSplit[0].trim(); // to trim the white space on bothsides
-					
+		        
+		            //arrayList for the products in the array
 		            List vegNeededList = Arrays.asList(vegNeeded);		// 8. convert array to arraylist - array takes less memory - at run time// coverting to arraylist --google the conversion
 					
-		            int j = 0; // to break the for loop once the products in the array are added to cart use a counter
-					if (vegNeededList.contains(vegName)) // compare the name -- to check for the required product
+		          	if (vegNeededList.contains(vegName)) // compare the name -- to check for the required product
 					{
 						j++;
 						System.out.println("The products added to cart count is " +j);
@@ -89,3 +66,30 @@ public class greenkartpractice {
 
 		
 }
+
+//1. To Locate the elements to add use findelements-- List of elements having the css -- a list
+	//2. products.size(); >> gives the size/count of products in the list
+	//3. getting name of product at index i
+	//4. Cucumber - 1 Kg - 3& 4 --type the name and compare the name --use ignoreCase or contains --As name is with 1kg its better not to use ignoreCase
+		 // compare the name -- to check for the required product 			 * 
+	//5. click on Add to Cart 
+	//6.  break; // once the required product is added --no need to check for the remaining // prodcuts -- break the for loop 
+	//7. if more no of products to add or to add more prodcuts any time -- to make the program generic use array to store the products-- keep on adding products
+			// here as needed-- no change required to the code --just add the product names here
+	//8. 4 to 8 convert array to arraylist - array takes less memory so storing the required products in an Array  -
+	     //and at run time coverting to arraylist as its easier to get the products--google the conversion
+	//9. //4 to 9- check whether name you extracted is present in arrayList or not// Arraylist also has contains method
+	//10. 5 to 10 //click on Add to cart
+	//11. Cucumber - 1 Kg -the product name in application is like these- so add // like these in the products array or split it
+	         //split returns 2 values in array -- returns an array with 2 values -- with right value and left value
+				// name = Cucumber - 1 Kg		// name1[0] = Cucumber	// name1[1] = 1 Kg
+	//12. to trim the white space on bothsides
+	//13. //9 to 13 compare the name -- to check for the required product
+	//14. //6 to 14 -- once the required product is added --no need to check for the remaining prodcuts -- to break the for loop use a counter
+	      //size of the array - no of required products in the array{break; 
+	      //once the required product is added - to break the for loop use a counter
+	//15.	10 to 15// xpath by text() -// but the text	// change once it is // clicked-- ADD TO CART to ADDED TO CART--SO ITS	// Better not to use text() -// locator text
+	      //ADD TO CART index is changed here when driver coming to locate the next product
+	//16. 	Thread.sleep(3000);
+
+	
